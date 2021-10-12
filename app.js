099d546822d4dbd1579ff3322b651b42d782254c
gursimran18 <kaurgursimran9918@gmail.com>
2021-10-12 20:49:49 +0530

    designed landing page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const path = require('path');
 const userRoutes = require('./Server/routes/userRoutes')
+const homeRoutes = require('./Server/routes/homeRoutes')
 app.use(morgan('dev'));
 
 // Middleware to read the body of http post request
@@ -13,12 +14,16 @@ app.use(express.urlencoded({
 }));
 
 //set view path 
+app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
-
+app.use("/static", express.static('static'));
 //specifying routes
+app.use('/home',homeRoutes);
+
 app.use('/users', userRoutes);
+
 
 app.use('*', (req, res) => {
     res.status(404).send('This route is not defined!');
