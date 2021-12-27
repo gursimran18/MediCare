f67ef58e254de6a84eade4bd7517895ac30a6358
Anjali Dwivedi <dwivedianjali10@gmail.com>
2021-12-27 18:19:52 +0530

    upload reports

--- Server/routes/patientRoutes.js
+++ Server/routes/patientRoutes.js
@@ -1,9 +1,172 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const {GridFsStorage} = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 var alert=require('alert');
 const router = express.Router();
+//const upload= require('express-fileupload')
 const Schedule = require("../models/scheduleModel");
 const doctors = require("../models/doctorModel");
 const lockedSchedule = require("../models/bookedModel");
+const connectDB = require('../database/connection')
+//const conn= connectDB();
+
+router.use(bodyParser.json());
+router.use(methodOverride('_method'));
+
+const mongoURI=process.env.MONGO_URI
+const conn = mongoose.createConnection(mongoURI);
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+
+// Create storage engine
+const storage = new GridFsStorage({
+    url: mongoURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  const upload = multer({ storage });
+  
+  // @route GET /
+  // @desc Loads form
+  router.get('/uploadFile', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        res.render('patients/uploadsView', { files: false });
+      } else {
+        files.map(file => {
+          if (
+            file.contentType === 'image/jpeg' ||
+            file.contentType === 'image/png'
+          ) {
+            file.isImage = true;
+          } else {
+            file.isImage = false;
+          }
+        });
+        res.render('patients/uploadsView', { files: files });
+      }
+    });
+  });
+  
+  // @route POST /upload
+  // @desc  Uploads file to DB
+  router.post('/uploadFile/upload', upload.single('file'), (req, res) => {
+    // res.json({ file: req.file });
+    res.redirect('/P_dashboard/uploadFile');
+  });
+  
+  // @route GET /files
+  // @desc  Display all files in JSON
+  router.get('/uploadFile/files', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+      // Check if files
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: 'No files exist'
+        });
+      }
+  
+      // Files exist
+      return res.json(files);
+    });
+  });
+  
+  // @route GET /files/:filename
+  // @desc  Display single file object
+  router.get('/uploadFile/files/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+      // File exists
+      return res.json(file);
+    });
+  });
+  
+  // @route GET /image/:filename
+  // @desc Display Image
+  router.get('/uploadFile/image/:filename', (req, res) => {
+    gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+      // Check if file
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists'
+        });
+      }
+  
+      // Check if image
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read output to browser
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image'
+        });
+      }
+    });
+  });
+  
+  // @route DELETE /files/:id
+  // @desc  Delete file
+  router.delete('/uploadFile/files/:id', (req, res) => {
+    gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+      if (err) {
+        return res.status(404).json({ err: err });
+      }
+  
+      res.redirect('/uploadFile');
+    });
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.get('',(req,res) =>{
     res.render('patients/dashboardPatient',{
         name: req.user.firstname
@@ -28,6 +191,47 @@ router.get('/profile',(req,res) =>{
 
 
     });
+})
+
+router.get('/p_viewAppointments',async(req,res) => {
+    const pat_id=req.user.id;
+    lockedSchedule.aggregate([
+        {
+            "$match":{
+                "patient_id": pat_id
+            }
+        },
+        {
+            "$lookup":{
+                "let": { "docObjId": { "$toObjectId": "$doctor_id" } },
+          "from":"doctors",
+          "pipeline":[
+            {"$match":{"$expr":{"$eq":[ "$_id","$$docObjId"]}}},
+            {"$project":{"firstname":1,"lastname":1, "email":1, "gender":1,"phone":1, "degree":1, "speciality":1}}
+          ],
+          "as":"DocData"
+        }},
+        {
+            "$lookup":{
+                "let": { "schedObjId": { "$toObjectId": "$schedule_id" } },
+          "from":"schedules",
+          "pipeline":[
+            {"$match":{"$expr":{"$eq":[ "$_id","$$schedObjId"]}}},
+            {"$project":{"date":1,"start_time":1, "end_time":1, "mode":1}}
+          ],
+          "as":"ScheduleData"
+        }}
+    ]).exec((err,result) => {
+        if(err){
+            res.send(err)
+        }
+        else{
+            console.log(result)
+            res.render('patients/p_viewAppointments',{
+                appointments: result
+             }) 
+        }
+    })
 })
 
  router.get('/book_appoint',(req,res, next)=>{
@@ -111,9 +315,34 @@ router.get('/profile',(req,res) =>{
       
     })
 
-    router.get('/joinCall',(req,res) =>{
+    router.get('/joinCall/:_id',(req,res) =>{
         res.render('patients/joinCall')
     })
+
+    router.get('/p_cancelAppointment/:_id',async(req,res)=>{
+        lockedSchedule.findByIdAndDelete(req.params._id.toString(),function(err,docs){
+            if(err){
+                res.send(err)
+            }
+            else{
+                res.redirect('/P_dashboard/p_viewAppointments')
+            }
+        })
+    })
+   
+        /*router.use(upload())
+
+    router.get('/uploadFile',(req,res) =>{
+        
+        res.render('patients/uploads');
+    })
+
+
+    router.post('/P_dashboard/uploadFilen',async(req,res) => {
+       if(req.files){
+           console.log(req.files)
+       }
+    })*/
 
 router.get('/logout',(req,res) =>{
     req.logout();
